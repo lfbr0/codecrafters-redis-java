9c374144534d786d84e9431aa02316e49c179ccc
Luís Filipe Ribeiro <20361894+lfbr0@users.noreply.github.com>
2026-04-03 16:07:22 +0100

    codecrafters submit [skip ci]

--- src/main/java/replication/ReplicationManager.java
+++ src/main/java/replication/ReplicationManager.java
@@ -1,11 +1,23 @@
 package replication;
 
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ReplicationManager {
 
     // state vars
     private static final AtomicBoolean isMaster = new AtomicBoolean(true);
+    private static final String masterReplId = generateMasterReplicationId();
+    private static final AtomicLong masterReplOffset = new AtomicLong(0);
+
+    /**
+     * Generates master replication id
+     * @return A 40-character alphanumeric string
+     */
+    private static String generateMasterReplicationId() {
+        return ("redis000" + UUID.randomUUID().toString().replaceAll("-",""));
+    }
 
     /**
      * Starts replication process given master host & port
@@ -25,7 +37,9 @@ public class ReplicationManager {
         return new StringBuilder()
                 // replication
                 .append("# Replication").append("\n")
-                .append("role:").append(isMaster.get() ? "master" : "slave")
+                .append("role:").append(isMaster.get() ? "master" : "slave").append("\n")
+                .append("master_replid:").append(masterReplId).append("\n")
+                .append("master_repl_offset:").append(masterReplOffset.get())
                 .toString();
     }
 
