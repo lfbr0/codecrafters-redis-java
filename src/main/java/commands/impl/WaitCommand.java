8efdc519c64d88c525c42af2121fd28ed58d5a14
Luís Filipe Ribeiro <20361894+lfbr0@users.noreply.github.com>
2026-04-11 00:52:18 +0100

    codecrafters submit [skip ci]

---
+++ src/main/java/commands/impl/WaitCommand.java
@@ -0,0 +1,38 @@
+package commands.impl;
+
+import commands.Command;
+import commands.CommandContext;
+import commands.CommandResponse;
+import logger.Logger;
+import serdes.RedisDeserializer;
+import serdes.RedisMessage;
+import serdes.RedisSerializer;
+
+import static serdes.RedisMessage.RedisMessageType.*;
+
+public class WaitCommand implements Command {
+    @Override
+    public CommandResponse execute(CommandContext context) throws Exception {
+        if (context.getArguments() == null || context.getArguments().size() != 2) {
+            throw new IllegalArgumentException("Wrong number of arguments!");
+        }
+
+        RedisMessage numReplicasRaw = context.getArguments().getFirst();
+        RedisMessage timeoutRaw = context.getArguments().getLast();
+        if (numReplicasRaw.getType() != timeoutRaw.getType() && timeoutRaw.getType() != INTEGER) {
+            throw new IllegalArgumentException("Arguments received were not of expected type!");
+        }
+
+        int numReplicas = Integer.parseInt(numReplicasRaw.getContent().toString());
+        long timeout = Long.parseLong(timeoutRaw.getContent().toString());
+        Logger.info("Performing wait command with numReplicas=" + numReplicas + ", timeout=" + timeout);
+
+        int ackedReplicas = 0;
+        return new CommandResponse(RedisSerializer.integer(ackedReplicas));
+    }
+
+    @Override
+    public boolean matches(String commandName) {
+        return "wait".equalsIgnoreCase(commandName);
+    }
+}
